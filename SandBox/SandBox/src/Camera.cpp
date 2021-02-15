7e497e4a3769a22026efece965c5a2a9a3740d81
Aksel Melby Haugen <akselh15@student.uia.no>
2021-02-15 22:01:59 +0100

    removd unused files

--- SandBox/SandBox/src/Camera.cpp
+++ SandBox/SandBox/src/Camera.cpp
@@ -3,7 +3,6 @@
 #include "imGui/imgui.h"
 
 #include "Window/Window.h"
-#include "Collision.h"
 
 Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
     : Front(glm::vec3(0.0f, 0.0f, -1.0f)), EyeWorld(glm::vec3(0.0f)),
--- SandBox/SandBox/src/Collision.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-bool fall(float& x, float& y, float& z, float m);
-
-class Collision
-{
-	Collision();
-	~Collision();
-};
